4d29baeab42431d884f8c5464e3161fa98f44118
Jordan Brown <31949071+jordanbrown0@users.noreply.github.com>
2020-03-01 22:14:18 -0800

    Fixed #59 server startup batch file should pause when the JS program fails

--- Program/lib/mkbat.bat
+++ Program/lib/mkbat.bat
@@ -10,5 +10,6 @@ set dest=%1
 set myname=%2
 
 set bat=%HOMEDRIVE%%HOMEPATH%\Desktop\%myname% Registration Server.bat
-echo @echo off      > "%bat%"
-echo %dest%\Server  >> "%bat%"
+echo @echo off             > "%bat%"
+echo call %dest%\Server    >> "%bat%"
+echo if errorlevel 1 pause >> "%bat%"
