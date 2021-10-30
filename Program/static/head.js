afbd69258bb31319c4695b4dd880b4c90f654b40
Jordan Brown <31949071+jordanbrown0@users.noreply.github.com>
2021-10-29 19:42:26 -0700

    Warn if you attempt to leave the application. Fixes #63.

--- Program/static/head.js
+++ Program/static/head.js
@@ -19,3 +19,9 @@ window.addEventListener('error', function(e) {
         });
     }
 });
+window.addEventListener('beforeunload', function (e) {
+  // Cancel the event
+  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  // Chrome requires returnValue to be set
+  e.returnValue = 'blah';
+});
