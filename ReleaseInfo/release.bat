bd249fa43dfdc99953daf6841abb5dfd065c3bce
Jordan Brown <31949071+jordanbrown0@users.noreply.github.com>
2021-11-24 10:50:09 -0800

    Release 2021-11-24 10:50:08

--- ReleaseInfo/release.bat
+++ ReleaseInfo/release.bat
@@ -1,2 +1,2 @@
-set token=Development 
-set release=Development 
+set token=2021-11-24.10.50.08 
+set release=2021-11-24 10:50:08 
--- ReleaseInfo/release.js
+++ ReleaseInfo/release.js
@@ -1,1 +1,1 @@
-var release='Development'; 
+var release='2021-11-24 10:50:08'; 
